2337a2d4dc962844b5ab07182d436aa621d8edba
tom_glb <goodMoring_glb@163.com>
2019-01-20 16:53:48 +0800

    sdfewqeqewq

--- mavenDao/src/main/java/com/maven/bean/User.java
+++ mavenDao/src/main/java/com/maven/bean/User.java
@@ -1,5 +1,27 @@
 package com.maven.bean;
 
 public class User {
+	
+	private String userName;
+	
+	private Integer id;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
 
 }
