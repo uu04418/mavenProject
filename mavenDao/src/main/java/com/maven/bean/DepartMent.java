b1cfd194d274b53386d138cf0b1a22f1d0ad8d4b
tom_glb <goodMoring_glb@163.com>
2019-01-20 17:02:27 +0800

    新建部落

---
+++ mavenDao/src/main/java/com/maven/bean/DepartMent.java
@@ -0,0 +1,29 @@
+package com.maven.bean;
+
+public class DepartMent {
+	
+	private Integer deptid;
+	
+	private String deptName;
+
+	public Integer getDeptid() {
+		return deptid;
+	}
+
+	public void setDeptid(Integer deptid) {
+		this.deptid = deptid;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	
+	
+	
+	
+
+}
